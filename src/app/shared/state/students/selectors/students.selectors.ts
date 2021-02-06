430ade0c0754c9c8096fa33ec951e9a578b7d35f
Isaac Alwar <31098369+isaac-alwar@users.noreply.github.com>
2021-02-06 17:20:10 +0000

    student module config:  TODO: Services

--- src/app/shared/state/students/selectors/students.selectors.ts
+++ src/app/shared/state/students/selectors/students.selectors.ts
@@ -4,3 +4,28 @@ import * as fromStudents from '../reducers/students.reducer';
 export const selectStudentsState = createFeatureSelector<fromStudents.State>(
   fromStudents.studentsFeatureKey
 );
+
+export const selectStudentIds = createSelector(
+  selectStudentsState,
+  fromStudents.selectIds
+);
+
+export const selectStudentEntities = createSelector(
+  selectStudentsState,
+  fromStudents.selectEntities
+);
+
+export const selectAllStudents = createSelector(
+  selectStudentsState,
+  fromStudents.selectAll
+);
+
+export const selectStudentTotal = createSelector(
+  selectStudentsState,
+  fromStudents.selectTotal
+);
+
+export const currentActiveStudentId = createSelector(
+  selectStudentsState,
+  fromStudents.getSelectedStudentId
+)
