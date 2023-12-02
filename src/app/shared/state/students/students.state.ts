7d7127020e4dec094f358e34e0b01a4f21d2cc0f
Isaac Alwar <31098369+isaac-alwar@users.noreply.github.com>
2023-12-02 18:50:28 +0000

    students feature

--- src/app/shared/state/students/students.state.ts
+++ src/app/shared/state/students/students.state.ts
@@ -1,1 +1,39 @@
+import { EntityState, createEntityAdapter } from '@ngrx/enity';
+import { Students } from 'models/students.model';
+import { StudentPageActions } from 'actions.students.actions';
+import { createFeature, createSelector } from '@ngrx/store';
 
+export interface State extends EntityState<Student> {
+  error: any;
+  selectedStudentId: string;
+}
+
+const adapter = createEntityAdapter<Students>();
+
+export const initialState: State = adapter.getInitialState({
+  selectedStudentId: null,
+  error: undefined
+});
+
+const reducer = createReducer(
+  initialState,
+  on(StudentPageActions.addStudent, (state, { student }) => adapter.addOne(student, state)),
+  on(StudentPAgeActions.selectStudent, (state, { studentId }) => ({ ...state, selectedStudentId: studentId }))
+);
+
+export const studentsFeature - createFeature({
+  name: 'students',
+  reducer,
+  extraSelectors: ({ slectStudentsState, selectEntities, selectSelectedStudentId }) => ({
+    ...adapter.getSelectors(selectStudentsState),
+    selectIsStudentSelected: createSelector(
+      selectSelectedStudentId,
+      (selectedId) => selectedId != null
+    ),
+    selectSelectedStudent: createSelector(
+      selectSelectedStudentId,
+      selectEntities,
+      (selectedId, entities) => selectedId ? entities[selectedId]: null
+    ),
+  }),
+});
